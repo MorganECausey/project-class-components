da11c43be59ee5e78bd05cdab4e0bc1bb9d4ce8b
Morgan Causey <morganelizabethcausey@gmail.com>
2023-12-06 15:31:12 -0500

    first item in list

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -3,6 +3,18 @@ import TodoList from './TodoList';
 import TodoForm from './Form';
 
 export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          name: 'Workout',
+          id: Date.now(),
+          completed: false
+        }, 
+      ]
+    }
+  }
   render() {
     return (
       <div>
