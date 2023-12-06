abf915aa90cf4ba1c8a8d9de38c6ac9a374a74a4
Morgan Causey <morganelizabethcausey@gmail.com>
2023-12-06 16:23:16 -0500

    app file complete, add notes later

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -2,6 +2,9 @@ import React from 'react'
 import TodoList from './TodoList';
 import TodoForm from './Form';
 
+function generateUniqueId() {
+  return Date.now();
+}
 export default class App extends React.Component {
   constructor() {
     super();
@@ -9,16 +12,65 @@ export default class App extends React.Component {
       todos: [
         {
           name: 'Workout',
-          id: Date.now(),
+          id: generateUniqueId(),
           completed: false
         }, 
+        {
+          name: 'Buy groceries',
+          id: generateUniqueId(),
+          completed: false
+        },
+        {
+          name: 'Give the dogs their baths',
+          id: generateUniqueId(),
+          completed: false
+        }
       ]
     }
   }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(element => {
+        return (element.completed === false);
+    })
+  });
+  }
+
+  handleAdd = (value) => {
+    this.setState({
+      ...this.state,
+      todos: [this.state.todos, {
+        name: value,
+        id: generateUniqueId(),
+        completed: false
+      }]
+    })
+  }
+
+  handleToggle = (clickedID) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(element => {
+        if (element.id === clickedID) {
+        return ({
+          ...element,
+          completed: !element.completed
+        })
+      }
+      return element;
+      })
+    })
+  }
   render() {
+    const { todos } = this.state
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+        <TodoList todos={todos} handleToggle={this.handleToggle}></TodoList>
+        <TodoForm handleAdd={this.handleAdd}></TodoForm>
+        <button onClick={this.handleClear}>Clear</button>
       </div>
     )
   }
