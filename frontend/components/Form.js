8869730febc6be4b9195119ab7b6ab283942f86d
Morgan Causey <morganelizabethcausey@gmail.com>
2023-12-06 17:17:18 -0500

    all files done, add notes later

--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,27 @@
 import React from 'react'
 
 export default class Form extends React.Component {
+
+  handleSumbit = (event) => {
+    event.preventDefault();
+    this.props.handleAdd(this.state.input);
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      ...this.state,
+      input: event.target.value
+    })
+  }
+
   render() {
     return (
-      <div>
-        Form
-      </div>
-    )
+      <form onClick={this.handleSumbit}>
+        <input
+        type="text"
+        onChange={this.handleChange}
+        />
+        <button>Add</button>
+        </form>);
   }
 }
--- frontend/components/TodoList.js
+++ frontend/components/TodoList.js
@@ -1,11 +1,14 @@
 import React from 'react'
-
+import Todo from './Todo'
 export default class TodoList extends React.Component {
+
   render() {
     return (
-      <div>
-        TodoList
-      </div>
+      <ul>
+        {this.props.todos.map(element => {
+          return (<Todo handleToggle={this.props.handleToggle} element={element} key={element.id}></Todo>)
+        })}
+      </ul>
     )
   }
 }
